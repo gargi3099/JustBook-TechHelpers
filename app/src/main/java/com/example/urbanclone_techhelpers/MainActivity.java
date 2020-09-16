a28f66017d983e86be9a235930fcd5c8f1066080
KritiVerma-20 <kritiv28@gmail.com>
2020-09-16 16:04:54 +0530

    Login page

--- app/src/main/java/com/example/urbanclone_techhelpers/MainActivity.java
+++ app/src/main/java/com/example/urbanclone_techhelpers/MainActivity.java
@@ -1,14 +1,32 @@
-package com.example.urbanclone_techhelpers;
+package com.example.application2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_SCREEN=5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
+
 }
---
+++ app/src/main/java/com/example/urbanclone_techhelpers/Register.java
@@ -0,0 +1,112 @@
+package com.example.application2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Register extends AppCompatActivity {
+    Button button;
+    EditText name,phn,email,pwd,cpwd,un,add;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().setTitle("REGISTER");
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
+        button = (Button) findViewById(R.id.btnregister);
+        name = (EditText)findViewById(R.id.etname);
+        email = (EditText)findViewById(R.id.etemail);
+        add = (EditText)findViewById(R.id.etaddress);
+        phn= (EditText)findViewById(R.id.etphone);
+        pwd = (EditText)findViewById(R.id.etPwd);
+        cpwd = (EditText)findViewById(R.id.etConfirmPwd);
+        un = (EditText)findViewById(R.id.etun);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validate()) {
+                    Toast.makeText(getBaseContext(), "Signup Successful", Toast.LENGTH_LONG).show();
+                    button.setEnabled(true);
+                    Intent intent = new Intent(Register.this, Login.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(getBaseContext(), "Signup Failed", Toast.LENGTH_LONG).show();
+                    button.setEnabled(true);
+                }
+            }
+        });
+    }
+    private final boolean validate() {
+        boolean valid = true;
+        boolean psisequal;
+
+        String n=name.getText().toString();
+        String e = email.getText().toString();
+        String p = pwd.getText().toString();
+        String cp = cpwd.getText().toString();
+        String a = add.getText().toString();
+        String u = un.getText().toString();
+        String mobile = phn.getText().toString();
+
+        if (n.isEmpty() || n.length() < 3) {
+            name.setError("enter at least 3 characters");
+            valid = false;
+        } else {
+            name.setError(null);
+        }
+        if(mobile.isEmpty() || mobile.length() < 10 || !mobile.matches("[0-9]{10}")) {
+            phn.setError("enter at least 3 characters");
+            valid = false;
+        } else {
+            phn.setError(null);
+        }
+        if (u.isEmpty() || u.length() < 3) {
+            un.setError("enter at least 3 characters");
+            valid = false;
+        } else {
+            un.setError(null);
+        }
+
+        if (a.isEmpty() ) {
+            add.setError("fill address field");
+            valid = false;
+        } else {
+            add.setError(null);
+        }
+        if (e.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(e).matches()) {
+            email.setError("enter a valid email address");
+            valid = false;
+        } else {
+            email.setError(null);
+        }
+
+        if (p.isEmpty() || p.length() < 4 || p.length() > 10) {
+            pwd.setError("range 4-10 alphanumeric characters");
+            valid = false;
+        }else {
+            pwd.setError(null);
+        }
+
+        if (p.equals(cp)){
+            cpwd.setError(null);
+            psisequal = true;
+        }else {
+            cpwd.setError("passwords do not match");
+            valid = false;
+            psisequal = false;
+        }
+
+        return valid;
+    }
+}
